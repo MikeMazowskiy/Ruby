ddced15bf7a74241342bcc3491ecb7220111db8a
Mike_Mazovskiy <vairelviper@gmail.com>
2021-06-20 12:33:48 +0200

    New task update

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
+
+  validates :username, presence: true, length: {minimum: 4, maximum: 250}
+  validates :body, presence: true, length: {minimum:5, maximum: 500}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :delete_all
   has_many :marks, dependent: :destroy
-  validates :title, presence: true, length: {minimum: 4}
+  validates :title, presence: true, length: {minimum: 4, maximum: 250}
+  validates :body, presence: true, length: {minimum:5, maximum: 500}
 end
